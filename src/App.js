d16693c15742d24599e3a0e9b1fd2c75dc034eec
miyo0150 <yousifmikael@gmail.com>
2025-01-27 22:22:12 +0100

    adding necessary files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import './components/layout'
+import DashboardLayoutBasic from './components/layout';
+
+function App() {
+  return (
+    <div className="App">
+      <DashboardLayoutBasic/>
+    </div>
+  );
+}
+export default App;
